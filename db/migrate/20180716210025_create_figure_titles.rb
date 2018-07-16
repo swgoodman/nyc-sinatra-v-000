4e757b8009f1ff0f00e05a72a11d41d56ed21f99
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 21:05:29 +0000

    automatically backed up by learn

--- db/migrate/20180716210025_create_figure_titles.rb
+++ db/migrate/20180716210025_create_figure_titles.rb
@@ -1,8 +1,8 @@
 class CreateFigureTitles < ActiveRecord::Migration
   def change
-    create_table do :figure_titles do |t|
-      t.integer :title_id
-      t.integer :figure_id
-    end
+    +    create_table :figure_titles do |t|
+  +      t.integer :title_id
+  +      t.integer :figure_id
+  +    end
   end
 end
