d3f3cea0e73595bf30102cfb1ff747478dfbb536
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 20:55:44 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,5 +3,7 @@ class CreateLandmarks < ActiveRecord::Migration
     create_table :landmarks do |t|
       t.string :name
       t.integer :figure_id
+      t.integer :year_completed
+    end
   end
 end
