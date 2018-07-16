0f416cacb5827840967954389230cb10decc0716
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 23:23:44 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,5 +6,16 @@ class FiguresController < ApplicationController
 
     post '/figures' do
       @figure = Figure.new(params[':figure'])
+     if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
     end
 end
