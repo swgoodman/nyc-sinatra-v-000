33240bd2b88144683a549aa3368fd4d2d03a0389
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 23:38:07 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -27,5 +27,22 @@ class FiguresController < ApplicationController
      @figure = Figure.find_by_id(params[:id])
 
      erb :'/figures/show'
-  end
+   end
+
+    patch '/figures/:id' do
+     @figure = Figure.find_by_id(params[:id])
+     @figure.update(params[:figure])
+ 
+     if !params[:title][:name].empty?
+       @figure.titles << Title.new(name: params[:title][:name])
+     end
+
+     if !params[:landmark][:name].empty?
+       @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year])
+     end
+
+     @figure.save
+
+     redirect to "/figures/#{@figure.id}"
+   end
 end
