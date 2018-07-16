fa8ad97c6c031b07fe9f4db1c48623d55da7678e
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-16 23:23:24 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,4 +3,8 @@ class FiguresController < ApplicationController
     get '/figures/new' do
       erb :'figures/new'
     end
+
+    post '/figures' do
+      @figure = Figure.new(params[':figure'])
+    end
 end
